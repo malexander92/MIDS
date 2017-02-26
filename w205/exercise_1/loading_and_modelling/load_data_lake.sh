310d83e9ff6126564759ded5eeafa5fe3a3bbadd
Michael Alexander <Malexander@comscore.com>
2017-02-26 01:04:59 -0500

    directry

--- w205/exercise_1/loading_and_modelling/load_data_lake.sh
+++ w205/exercise_1/loading_and_modelling/load_data_lake.sh
@@ -6,6 +6,7 @@
 #tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hvbp_hcahps_11_10_2016.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/survey_responses.csv
 
 # creating directories and loading files into hdfs
+hdfs dfs -mkdir /user/w205/hospital_compare
 hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
 hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hospitals.csv /user/w205/hospital_compare/hospitals
 
