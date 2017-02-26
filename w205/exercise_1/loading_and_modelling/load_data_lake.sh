b653157d756f979bc47329c46d830557a97eebf1
Michael Alexander <Malexander@comscore.com>
2017-02-26 00:14:08 -0500

    spaces

--- w205/exercise_1/loading_and_modelling/load_data_lake.sh
+++ w205/exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,8 +1,8 @@
 # renaming files and removing headers in /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles
-tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Hospital General Information.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hospitals.csv
-tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Timely and Effective Care - Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/effective_care.csv
-tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Readmissions and Deaths - Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/readmissions.csv
-tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Measure Dates.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/measures.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Hospital\ General\ Information.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hospitals.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Timely\ and\ Effective\ Care\ -\ Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/effective_care.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Readmissions\ and\ Deaths\ -\ Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/readmissions.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Measure\ Dates.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/measures.csv
 tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hvbp_hcahps_11_10_2016.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/survey_responses.csv
 
 # loading files into Hospital_Revised_Flatfiles
