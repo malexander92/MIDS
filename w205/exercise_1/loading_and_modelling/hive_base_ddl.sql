b20311519e98813b5e762fe1d41ce820c0a0c217
Michael Alexander <Malexander@comscore.com>
2017-02-26 01:58:57 -0500

    sql tables

--- w205/exercise_1/loading_and_modelling/hive_base_ddl.sql
+++ w205/exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -1,14 +1,14 @@
 -- creating hospitals table
 DROP TABLE IF EXISTS hospitals;
 CREATE EXTERNAL TABLE hospitals (
-	provider_id	INT,
+	provider_id	STRING,
 	hospital_name STRING,
 	address STRING,
 	city STRING,
 	state STRING,
-	zip_code INT,
+	zip_code STRING,
 	county_name STRING,
-	phone_number INT,
+	phone_number STRING,
 	hospital_type STRING,
 	hospital_ownership STRING,
 	emergency_services STRING,
@@ -30,7 +30,12 @@ CREATE EXTERNAL TABLE hospitals (
 	efficient_imaging_comp STRING, --Efficient_use_of_medical_imaging_national_comparison
 	efficient_imaging_comp_note STRING --Efficient_use_of_medical_imaging_national_comparison_footnote
 )
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/hospitals/'
 ;
@@ -38,24 +43,29 @@ LOCATION '/user/w205/hospital_compare/hospitals/'
 -- creating effective_care table
 DROP TABLE IF EXISTS effective_care;
 CREATE EXTERNAL TABLE effective_care (
-	provider_id	INT,
+	provider_id	STRING,
 	hospital_name STRING,
 	address STRING,
 	city STRING,
 	state STRING,
-	zip_code INT,
+	zip_code STRING,
 	county_name STRING,
-	phone_number INT,
+	phone_number STRING,
 	condition STRING,
 	measure_id STRING,
 	measure_name STRING,
 	score STRING,
 	sample STRING,
 	footnote STRING,
-	measure_start_date DATE,
-	measure_end_date DATE
+	measure_start_date STRING,
+	measure_end_date STRING
 )
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/effective_care/'
 ;
@@ -66,11 +76,16 @@ CREATE EXTERNAL TABLE measures (
 	measure_name STRING,
 	measure_id STRING,
 	measure_start_quarter STRING,
-	measure_start_date DATE,
+	measure_start_date STRING,
 	measure_end_quarter STRING,
-	measure_end_date DATE
+	measure_end_date STRING
 )
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/measures/'
 ;
@@ -78,25 +93,117 @@ LOCATION '/user/w205/hospital_compare/measures/'
 -- creating readmissions table
 DROP TABLE IF EXISTS readmissions;
 CREATE EXTERNAL TABLE readmissions (
-	provider_id	INT,
+	provider_id	STRING,
 	hospital_name STRING,
 	address STRING,
 	city STRING,
 	state STRING,
-	zip_code INT,
+	zip_code STRING,
 	county_name STRING,
-	phone_number INT,
+	phone_number STRING,
 	measure_name STRING,
 	measure_id STRING,
 	compared_to_national  STRING,
-	denominator INT,
-	score DECIMAL,
-	lower_estimate DECIMAL,
+	denominator STRING,
+	score STRING,
+	lower_estimate STRING,
 	higher_estimate_footnote STRING,
-	measure_start_date DATE,
-	measure_end_date DATE
+	measure_start_date STRING,
+	measure_end_date STRING
 )
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/readmissions/'
+;
+
+-- creating survey_responses table
+DROP TABLE IF EXISTS survey_responses;
+CREATE EXTERNAL TABLE survey_responses (
+	provider_number	STRING,
+	hospital_name STRING,
+	address STRING,
+	city STRING,
+	state STRING,
+	zip_code STRING,
+	county_name STRING,
+	communication_with_nurses_floor STRING,
+	communication_with_nurses_achievement_threshold STRING,
+	communication_with_nurses_benchmark STRING,
+	communication_with_nurses_baseline_rate STRING,
+	communication_with_nurses_performance_rate STRING,
+	communication_with_nurses_achievement_points STRING,
+	communication_with_nurses_improvement_points STRING,
+	communication_with_nurses_dimension_score STRING,
+	communication_with_doctors_floor STRING,
+	communication_with_doctors_achievement_threshold STRING,
+	communication_with_doctors_benchmark STRING,
+	communication_with_doctors_baseline_rate STRING,
+	communication_with_doctors_performance_rate STRING,
+	communication_with_doctors_achievement_points STRING,
+	communication_with_doctors_improvement_points STRING,
+	communication_with_doctors_dimension_score STRING,
+	responsiveness_of_hospital_staff_floor STRING,
+	responsiveness_of_hospital_staff_achievement_threshold STRING,
+	responsiveness_of_hospital_staff_benchmark STRING,
+	responsiveness_of_hospital_staff_baseline_rate STRING,
+	responsiveness_of_hospital_staff_performance_rate STRING,
+	responsiveness_of_hospital_staff_achievement_points STRING,
+	responsiveness_of_hospital_staff_improvement_points STRING,
+	responsiveness_of_hospital_staff_dimension_score STRING,
+	pain_management_floor STRING,
+	pain_management_achievement_threshold STRING,
+	pain_management_benchmark STRING,
+	pain_management_baseline_rate STRING,
+	pain_management_performance_rate STRING,
+	pain_management_achievement_points STRING,
+	pain_management_improvement_points STRING,
+	pain_management_dimension_score STRING,
+	communication_about_medicines_floor STRING,
+	communication_about_medicines_achievement_threshold STRING,
+	communication_about_medicines_benchmark STRING,
+	communication_about_medicines_baseline_rate STRING,
+	communication_about_medicines_performance_rate STRING,
+	communication_about_medicines_achievement_points STRING,
+	communication_about_medicines_improvement_points STRING,
+	communication_about_medicines_dimension_score STRING,
+	cleanliness_and_quietness_of_hospital_environment_floor STRING,
+	cleanliness_and_quietness_of_hospital_environment_achievement_threshold STRING,
+	cleanliness_and_quietness_of_hospital_environment_benchmark STRING,
+	cleanliness_and_quietness_of_hospital_environment_baseline_rate STRING,
+	cleanliness_and_quietness_of_hospital_environment_performance_rate STRING,
+	cleanliness_and_quietness_of_hospital_environment_achievement_points STRING,
+	cleanliness_and_quietness_of_hospital_environment_improvement_points STRING,
+	cleanliness_and_quietness_of_hospital_environment_dimension_score STRING,	
+	discharge_information_floor STRING,
+	discharge_information_achievement_threshold STRING,
+	discharge_information_benchmark STRING,
+	discharge_information_baseline_rate STRING,
+	discharge_information_performance_rate STRING,
+	discharge_information_achievement_points STRING,
+	discharge_information_improvement_points STRING,
+	discharge_information_dimension_score STRING,
+	overall_rating_of_hospital_floor STRING,
+	overall_rating_of_hospital_achievement_threshold STRING,
+	overall_rating_of_hospital_benchmark STRING,
+	overall_rating_of_hospital_baseline_rate STRING,
+	overall_rating_of_hospital_performance_rate STRING,
+	overall_rating_of_hospital_achievement_points STRING,
+	overall_rating_of_hospital_improvement_points STRING,
+	overall_rating_of_hospital_dimension_score STRING,
+	hcahps_base_score STRING,
+	hcahps_consistency_score STRING
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+	"separatorChar" = ",",
+	"quoteChar" = '"',
+	"escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/survey_responses/'
 ;
