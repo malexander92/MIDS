1999fe08cbf76c4d662694076d810d0f840fec1e
Michael Alexander <Malexander@comscore.com>
2017-02-26 14:22:40 -0500

    more files

--- w205/exercise_1/loading_and_modelling/load_data_lake.sh
+++ w205/exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,7 +1,13 @@
 # renaming files and removing headers in /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles
 tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Hospital\ General\ Information.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hospitals.csv
-tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Timely\ and\ Effective\ Care\ -\ Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/effective_care.csv
-tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Readmissions\ and\ Deaths\ -\ Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/readmissions.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Timely\ and\ Effective\ Care\ -\ Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/effective_care_hospital.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Timely\ and\ Effective\ Care\ -\ State.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/effective_care_state.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Readmissions\ and\ Deaths\ -\ Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/readmissions_hospital.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Readmissions\ and\ Deaths\ -\ State.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/readmissions_state.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Medicare\ Hospital\ Spending\ per\ Patient\ -\ Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/spending_hospital.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Medicare\ Hospital\ Spending\ per\ Patient\ -\ State.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/spending_state.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Healthcare\ Associated\ Infections\ -\ Hospital.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hai_hospital.csv
+tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Healthcare\ Associated\ Infections\ -\ State.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hai_state.csv
 tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/Measure\ Dates.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/measures.csv
 tail -n +2 /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hvbp_hcahps_11_10_2016.csv > /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/survey_responses.csv
 
@@ -10,11 +16,29 @@ hdfs dfs -mkdir /user/w205/hospital_compare
 hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
 hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hospitals.csv /user/w205/hospital_compare/hospitals
 
-hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
-hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/effective_care.csv /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care_hospital
+hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/effective_care_hospital.csv /user/w205/hospital_compare/effective_care_hospital
 
-hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
-hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/readmissions.csv /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care_state
+hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/effective_care_state.csv /user/w205/hospital_compare/effective_care_state
+
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions_hospital
+hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/readmissions_hospital.csv /user/w205/hospital_compare/readmissions_hospital
+
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions_state
+hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/readmissions_state.csv /user/w205/hospital_compare/readmissions_state
+
+hdfs dfs -mkdir /user/w205/hospital_compare/spending_hospital
+hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/spending_hospital.csv /user/w205/hospital_compare/spending_hospital
+
+hdfs dfs -mkdir /user/w205/hospital_compare/spending_state
+hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/spending_state.csv /user/w205/hospital_compare/spending_state
+
+hdfs dfs -mkdir /user/w205/hospital_compare/hai_hospital
+hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hai_hospital.csv /user/w205/hospital_compare/hai_hospital
+
+hdfs dfs -mkdir /user/w205/hospital_compare/hai_state
+hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/hai_state.csv /user/w205/hospital_compare/hai_state
 
 hdfs dfs -mkdir /user/w205/hospital_compare/measures
 hdfs dfs -put /home/w205/MIDS/w205/exercise_1/Hospital_Revised_Flatfiles/measures.csv /user/w205/hospital_compare/measures
