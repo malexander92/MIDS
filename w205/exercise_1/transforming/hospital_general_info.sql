11491bb3d4977d370116653422499599c825861d
Michael Alexander <Malexander@comscore.com>
2017-02-26 21:52:02 -0500

    sql

---
+++ w205/exercise_1/transforming/hospital_general_info.sql
@@ -0,0 +1,17 @@
+-- creating tansformed hospital general info table
+DROP TABLE IF EXISTS hospital_general_info;
+CREATE TABLE hospital_general_info AS
+SELECT
+	CAST(provider_id AS INT),
+	hospital_name,
+	address,
+	city,
+	state,
+	CAST(zip_code AS INT),
+	county_name,
+	CAST(phone_number AS INT),
+	hospital_type,
+	hospital_ownership,
+	emergency_services,
+FROM hospitals
+;
