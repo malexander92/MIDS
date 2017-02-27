f37c52705aadf1d770767ef84d18e6ab1206e402
Michael Alexander <Malexander@comscore.com>
2017-02-26 21:30:08 -0500

    sql

--- w205/exercise_1/transforming/hospitals_table.sql
+++ w205/exercise_1/transforming/hospitals_table.sql
@@ -2,22 +2,22 @@
 DROP TABLE IF EXISTS hospital_general_ratings;
 CREATE TABLE hospital_general_ratings AS
 SELECT
-	provider_id,
-	state,
+	provider_id INT,
+	state VARCHAR,
 	CASE WHEN meets_ehr_criteria = 'Y'
 		THEN 1 
 		WHEN meets_ehr_criteria IS NULL THEN 0
 		ELSE 0 END
-		AS meets_ehr_criteria,
+		AS meets_ehr_criteria INT,
 	CASE WHEN hospital_rating = 'Not Available'
 		THEN NULL ELSE hospital_rating END
-		AS hospital_rating,
-	mortality_comp,
-	safety_care_comp,
-	readmission_comp,
-	pat_exp_comp,
-	effective_care_comp,
-	timeliness_care_comp,
-	efficient_imaging_comp
+		AS hospital_rating INT,
+	mortality_comp VARCHAR,
+	safety_care_comp VARCHAR,
+	readmission_comp VARCHAR,
+	pat_exp_comp VARCHAR,
+	effective_care_comp VARCHAR,
+	timeliness_care_comp VARCHAR,
+	efficient_imaging_comp VARCHAR
 FROM hospitals
 ;
