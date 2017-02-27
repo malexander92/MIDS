9401396b303332307e11b09dfd7111e57e1130ae
Michael Alexander <Malexander@comscore.com>
2017-02-26 23:03:00 -0500

    run  sql scripts

---
+++ w205/exercise_1/transforming/run_all_sql_scripts.sh
@@ -0,0 +1,11 @@
+hive -f effective_care_hospitals_scores.sql
+hive -f effective_care_state_scores.sql
+hive -f hai_hospitals_scores.sql
+hive -f hai_state_scores.sql
+hive -f hospital_general_info.sql
+hive -f hospital_general_ratings.sql
+hive -f measure_map.sql
+hive -f readmissions_hospitals_scores.sql
+hive -f readmissions_state_scores.sql
+hive -f spending_hospitals_scores.sql
+hive -f spending_state_scores.sql
