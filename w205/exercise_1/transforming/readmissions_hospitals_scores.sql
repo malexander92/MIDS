b22c95ae34265e471906fd931f9bcb61598b39a0
Michael Alexander <Malexander@comscore.com>
2017-02-27 02:26:46 -0500

    s

--- w205/exercise_1/transforming/readmissions_hospitals_scores.sql
+++ w205/exercise_1/transforming/readmissions_hospitals_scores.sql
@@ -18,5 +18,9 @@ SELECT
 		WHEN measure_id ILIKE 'MORT%' THEN 'mortality_average' END
 		AS measure_group,
 	SUM(score)/COUNT(*)	
-FROM readmissions_hospital
+FROM readmissions_hospitals_scores
+GROUP BY
+	provider_id,
+	CASE WHEN measure_id ILIKE 'READM%' THEN 'readmission_average'
+		WHEN measure_id ILIKE 'MORT%' THEN 'mortality_average' END
 ;
