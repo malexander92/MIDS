b33a9b77933963662c6ca2350a3c6cb315298113
Michael Alexander <Malexander@comscore.com>
2017-02-26 22:35:25 -0500

    sql

---
+++ w205/exercise_1/transforming/effective_care_hospitals_scores.sql
@@ -0,0 +1,13 @@
+-- creating tansformed effectiveness of care hospital score table
+DROP TABLE IF EXISTS effective_care_hospitals_scores;
+CREATE TABLE effective_care_hospitals_scores AS
+SELECT
+	CAST(provider_id AS INT),
+	measure_id,
+	condition,
+	CAST((CASE WHEN score = 'Not Available'
+		THEN NULL ELSE score END) AS INT)
+		AS score
+FROM effective_care_hospital
+WHERE measure_id != 'EDV'
+;
---
+++ w205/exercise_1/transforming/effective_care_state_scores.sql
@@ -0,0 +1,11 @@
+-- creating tansformed effectiveness of care state score table
+DROP TABLE IF EXISTS effective_care_state_scores;
+CREATE TABLE effective_care_state_scores AS
+SELECT
+	state,
+	measure_id,
+	CAST((CASE WHEN score = 'Not Available'
+		THEN NULL ELSE score END) AS INT)
+		AS score
+FROM effective_care_state
+;
