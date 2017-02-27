e1dbc248fbc86b2436f43d37ff61f256cedce30d
Michael Alexander <Malexander@comscore.com>
2017-02-27 14:14:47 -0500

    best hospitals sql

--- w205/exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -109,9 +109,16 @@ SELECT
 	*
 FROM best_hospitals
 WHERE better_general_comparison_count > 1
-AND worse_general_comparison_count < 2
+AND worse_general_comparison_count = 0
 AND worse_hai_measure_count = 0
 AND better_hai_measure_count > 0
-AND readmission_average <= 15
-AND mortality_average <= 15
+AND readmission_average <= 12
+AND mortality_average <= 12
+AND mortality_comp NOT LIKE 'Not Available'
+AND safety_care_comp NOT LIKE 'Not Available'
+AND readmission_comp NOT LIKE 'Not Available'
+AND pat_exp_comp NOT LIKE 'Not Available'
+AND effective_care_comp NOT LIKE 'Not Available'
+AND timeliness_care_comp NOT LIKE 'Not Available'
+AND efficient_imaging_comp NOT LIKE 'Not Available'
 ;
--- w205/exercise_1/investigations/best_hospitals/top_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/top_hospitals.sql
@@ -9,4 +9,11 @@ AND worse_hai_measure_count = 0
 AND better_hai_measure_count > 0
 AND readmission_average <= 12
 AND mortality_average <= 12
+AND mortality_comp NOT LIKE 'Not Available'
+AND safety_care_comp NOT LIKE 'Not Available'
+AND readmission_comp NOT LIKE 'Not Available'
+AND pat_exp_comp NOT LIKE 'Not Available'
+AND effective_care_comp NOT LIKE 'Not Available'
+AND timeliness_care_comp NOT LIKE 'Not Available'
+AND efficient_imaging_comp NOT LIKE 'Not Available'
 ;
