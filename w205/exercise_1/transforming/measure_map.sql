529890724ee68c7157c644a4af48030714d1e5da
Michael Alexander <Malexander@comscore.com>
2017-02-27 02:00:10 -0500

    s

--- w205/exercise_1/transforming/measure_map.sql
+++ w205/exercise_1/transforming/measure_map.sql
@@ -7,3 +7,91 @@ SELECT
 FROM measures
 GROUP BY measure_id,measure_name
 ;
+
+INSERT INTO measure_map
+SELECT
+	a.measure_id,
+	a.measure_name
+FROM effective_care_hospital a
+LEFT OUTER JOIN measure_map b
+ON a.measure_id = b.measure_id
+AND b.measure_id IS NULL
+GROUP BY a.measure_id,a.measure_name
+;
+
+INSERT INTO measure_map
+SELECT
+	a.measure_id,
+	a.measure_name
+FROM effective_care_state a
+LEFT OUTER JOIN measure_map b
+ON a.measure_id = b.measure_id
+AND b.measure_id IS NULL
+GROUP BY a.measure_id,a.measure_name
+;
+
+INSERT INTO measure_map
+SELECT
+	a.measure_id,
+	a.measure_name
+FROM readmissions_hospital a
+LEFT OUTER JOIN measure_map b
+ON a.measure_id = b.measure_id
+AND b.measure_id IS NULL
+GROUP BY a.measure_id,a.measure_name
+;
+
+INSERT INTO measure_map
+SELECT
+	a.measure_id,
+	a.measure_name
+FROM readmissions_state a
+LEFT OUTER JOIN measure_map b
+ON a.measure_id = b.measure_id
+AND b.measure_id IS NULL
+GROUP BY a.measure_id,a.measure_name
+;
+
+INSERT INTO measure_map
+SELECT
+	a.measure_id,
+	a.measure_name
+FROM spending_hospital a
+LEFT OUTER JOIN measure_map b
+ON a.measure_id = b.measure_id
+AND b.measure_id IS NULL
+GROUP BY a.measure_id,a.measure_name
+;
+
+INSERT INTO measure_map
+SELECT
+	a.measure_id,
+	a.measure_name
+FROM spending_state a
+LEFT OUTER JOIN measure_map b
+ON a.measure_id = b.measure_id
+AND b.measure_id IS NULL
+GROUP BY a.measure_id,a.measure_name
+;
+
+INSERT INTO measure_map
+SELECT
+	a.measure_id,
+	a.measure_name
+FROM hai_hospital a
+LEFT OUTER JOIN measure_map b
+ON a.measure_id = b.measure_id
+AND b.measure_id IS NULL
+GROUP BY a.measure_id,a.measure_name
+;
+
+INSERT INTO measure_map
+SELECT
+	a.measure_id,
+	a.measure_name
+FROM hai_state a
+LEFT OUTER JOIN measure_map b
+ON a.measure_id = b.measure_id
+AND b.measure_id IS NULL
+GROUP BY a.measure_id,a.measure_name
+;
--- w205/exercise_1/transforming/run_all_sql_scripts.sh
+++ w205/exercise_1/transforming/run_all_sql_scripts.sh
@@ -1,12 +1,13 @@
+# running all sql scripts
 hive -f effective_care_hospitals_scores.sql
 hive -f effective_care_state_scores.sql
 hive -f hai_hospitals_scores.sql
 hive -f hai_state_scores.sql
 hive -f hospital_general_info.sql
 hive -f hospital_general_ratings.sql
-hive -f measure_map.sql
 hive -f readmissions_hospitals_scores.sql
 hive -f readmissions_state_scores.sql
 hive -f spending_hospitals_scores.sql
 hive -f spending_state_scores.sql
 hive -f survey_response_data.sql
+hive -f measure_map.sql
