02239a7bc9693221371c0f3cf550f38393650134
Michael Alexander <Malexander@comscore.com>
2017-02-27 16:13:16 -0500

    sql

--- w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
@@ -149,42 +149,32 @@ SELECT
 	a.provider_id,
 	a.hospital_name,
 	a.readmission_agg_score,
-	b.pop_mean_score AS readmission_agg_pop_mean,
-	b.pop_max_score AS readmission_agg_pop_max,
-	b.pop_min_score AS readmission_agg_pop_min,
-	b.pop_std_score AS readmission_agg_pop_std,
+	(SELECT pop_mean_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'readmission_average') AS readmission_agg_pop_mean,
+	(SELECT pop_max_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'readmission_average') AS readmission_agg_pop_max,
+	(SELECT pop_min_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'readmission_average') AS readmission_agg_pop_min,
+	(SELECT pop_std_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'readmission_average') AS readmission_agg_pop_std,
 	a.mortality_agg_score,
-	c.pop_mean_score AS mortality_agg_pop_mean,
-	c.pop_max_score AS mortality_agg_pop_max,
-	c.pop_min_score AS mortality_agg_pop_min,
-	c.pop_std_score AS mortality_agg_pop_std,
+	(SELECT pop_mean_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'mortality_average') AS mortality_agg_pop_mean,
+	(SELECT pop_max_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'mortality_average') AS mortality_agg_pop_max,
+	(SELECT pop_min_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'mortality_average') AS mortality_agg_pop_min,
+	(SELECT pop_std_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'mortality_average') AS mortality_agg_pop_std,
 	a.better_hai_measure_count,
-	d.better_hai_pop_mean,
-	d.better_hai_pop_max,
-	d.better_hai_pop_min,
+	(SELECT better_hai_pop_mean FROM hai_hospitals_scores_agg_pop_stats) AS better_hai_pop_mean,
+	(SELECT better_hai_pop_max FROM hai_hospitals_scores_agg_pop_stats) AS better_hai_pop_max,
+	(SELECT better_hai_pop_min FROM hai_hospitals_scores_agg_pop_stats) AS better_hai_pop_min,
 	a.worse_hai_measure_count,
-	d.worse_hai_pop_mean,
-	d.worse_hai_pop_max,
-	d.worse_hai_pop_min,
+	(SELECT worse_hai_pop_mean FROM hai_hospitals_scores_agg_pop_stats) AS worse_hai_pop_mean,
+	(SELECT worse_hai_pop_max FROM hai_hospitals_scores_agg_pop_stats) AS worse_hai_pop_max,
+	(SELECT worse_hai_pop_min FROM hai_hospitals_scores_agg_pop_stats) AS worse_hai_pop_min,
 	a.better_general_comparison_count,
-	e.better_general_pop_mean,
-	e.better_general_pop_max,
-	e.better_general_pop_min,
+	(SELECT better_general_pop_mean FROM hospitals_general_agg_pop_stats) AS better_general_pop_mean,
+	(SELECT better_general_pop_max FROM hospitals_general_agg_pop_stats) AS better_general_pop_max,
+	(SELECT better_general_pop_min FROM hospitals_general_agg_pop_stats) AS better_general_pop_min,
 	a.worse_general_comparison_count,
-	e.worse_general_pop_mean,
-	e.worse_general_pop_max,
-	e.worse_general_pop_min
+	(SELECT worse_general_pop_mean FROM hospitals_general_agg_pop_stats) AS worse_general_pop_mean,
+	(SELECT worse_general_pop_max FROM hospitals_general_agg_pop_stats) AS worse_general_pop_max,
+	(SELECT worse_general_pop_min FROM hospitals_general_agg_pop_stats) AS worse_general_pop_min
 FROM best_hospitals a
-JOIN readmissions_hospitals_scores_pop_stats b
-	ON 1 = 1
-	AND b.measure_group = 'readmission_average'
-JOIN readmissions_hospitals_scores_pop_stats c
-	ON 1 = 1
-	AND c.measure_group = 'mortality_average'
-JOIN hai_hospitals_scores_agg_pop_stats d
-	ON 1 = 1
-JOIN hospitals_general_agg_pop_stats e
-	ON 1 = 1
 WHERE a.better_general_comparison_count > 1
 	AND a.worse_general_comparison_count = 0
 	AND a.worse_hai_measure_count = 0
