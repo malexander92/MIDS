34eef9219681c526e4aee9a67adcdc3652920628
Michael Alexander <Malexander@comscore.com>
2017-02-27 16:55:27 -0500

    sql

--- w205/exercise_1/investigations/best_hospitals/top_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/top_hospitals.sql
@@ -42,8 +42,8 @@ SELECT
  	AND a.worse_general_comparison_count = 0
  	AND a.worse_hai_measure_count = 0
  	AND a.better_hai_measure_count > 0
- 	AND a.readmission_average <= 12
- 	AND a.mortality_average <= 12
+ 	AND a.readmission_agg_score <= 12
+ 	AND a.mortality_agg_score <= 12
  	AND a.mortality_comp NOT LIKE 'Not Available'
  	AND a.safety_care_comp NOT LIKE 'Not Available'
  	AND a.readmission_comp NOT LIKE 'Not Available'
