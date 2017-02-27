5cc24b5f89c2089ad75e0d85f3f7d0724501c6c9
Michael Alexander <Malexander@comscore.com>
2017-02-27 17:39:03 -0500

    sql

--- w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
@@ -187,13 +187,13 @@ JOIN hospitals_general_agg_pop_stats e
 
 DROP TABLE IF EXISTS top_10_hospitals;
 CREATE TABLE top_10_hospitals AS
-SELECT
-	*
+SELECT  
+        *
 FROM top_hospitals
-WHERE readmission_agg_score > readmission_agg_pop_mean
-AND mortality_agg_score > mortality_agg_pop_mean
-AND better_hai_measure_count > better_hai_pop_mean
-AND better_general_comparison_count > better_general_pop_mean
+WHERE readmission_agg_score < readmission_agg_pop_mean
+AND mortality_agg_score < mortality_agg_pop_mean
+AND better_hai_measure_count = better_hai_pop_max
+AND better_general_comparison_count = better_general_pop_max
 AND worse_hai_measure_count < worse_hai_pop_mean
 AND worse_general_comparison_count < worse_general_pop_mean
 ;
