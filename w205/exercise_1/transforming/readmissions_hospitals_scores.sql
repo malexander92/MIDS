407801591c9feb281201193000c70bc540ea7e67
Michael Alexander <Malexander@comscore.com>
2017-02-27 02:49:31 -0500

    s

--- w205/exercise_1/transforming/readmissions_hospitals_scores.sql
+++ w205/exercise_1/transforming/readmissions_hospitals_scores.sql
@@ -9,18 +9,3 @@ SELECT
 		AS score
 FROM readmissions_hospital
 ;
-
-DROP TABLE IF EXISTS readmissions_hospitals_scores_average;
-CREATE TABLE readmissions_hospitals_scores_average AS
-SELECT
-	provider_id,
-	CASE WHEN measure_id LIKE 'READM%' THEN 'readmission_average'
-		WHEN measure_id LIKE 'MORT%' THEN 'mortality_average' END
-		AS measure_group,
-	SUM(score)/COUNT(*)	AS average_score
-FROM readmissions_hospitals_scores
-GROUP BY
-	provider_id,
-	CASE WHEN measure_id LIKE 'READM%' THEN 'readmission_average'
-		WHEN measure_id LIKE 'MORT%' THEN 'mortality_average' END
-;
