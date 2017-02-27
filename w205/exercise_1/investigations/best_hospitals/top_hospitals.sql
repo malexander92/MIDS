235e166dbf43c33e9f00bf9240249a9c088656e8
Michael Alexander <Malexander@comscore.com>
2017-02-27 16:38:47 -0500

    sql

--- w205/exercise_1/investigations/best_hospitals/top_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/top_hospitals.sql
@@ -25,31 +25,31 @@ WHERE a.better_general_comparison_count > 1
 	AND a.efficient_imaging_comp NOT LIKE 'Not Available'
 ;
 
-ALTER TABLE top_hospitals ADD COLUMN readmission_agg_pop_mean DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN readmission_agg_pop_max DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN readmission_agg_pop_min DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN readmission_agg_pop_std DECIMAL;
+ALTER TABLE top_hospitals ADD COLUMNS (readmission_agg_pop_mean DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (readmission_agg_pop_max DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (readmission_agg_pop_min DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (readmission_agg_pop_std DECIMAL);
 
 UPDATE top_hospitals
 SET readmission_agg_pop_mean = (SELECT pop_mean_score FROM readmissions_hospitals_scores_pop_stats WHERE measure_group = 'readmission_average');
 
-ALTER TABLE top_hospitals ADD COLUMN mortality_agg_pop_mean DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN mortality_agg_pop_max DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN mortality_agg_pop_min DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN mortality_agg_pop_std DECIMAL;
+ALTER TABLE top_hospitals ADD COLUMNS (mortality_agg_pop_mean DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (mortality_agg_pop_max DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (mortality_agg_pop_min DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (mortality_agg_pop_std DECIMAL);
 
-ALTER TABLE top_hospitals ADD COLUMN better_hai_pop_mean DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN better_hai_pop_max DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN better_hai_pop_min DECIMAL;
+ALTER TABLE top_hospitals ADD COLUMNS (better_hai_pop_mean DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (better_hai_pop_max DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (better_hai_pop_min DECIMAL);
 
-ALTER TABLE top_hospitals ADD COLUMN worse_hai_pop_mean DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN worse_hai_pop_max DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN worse_hai_pop_min DECIMAL;
+ALTER TABLE top_hospitals ADD COLUMNS (worse_hai_pop_mean DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (worse_hai_pop_max DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (worse_hai_pop_min DECIMAL);
 
-ALTER TABLE top_hospitals ADD COLUMN better_general_pop_mean DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN better_general_pop_max DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN better_general_pop_min DECIMAL;
+ALTER TABLE top_hospitals ADD COLUMNS (better_general_pop_mean DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (better_general_pop_max DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (better_general_pop_min DECIMAL);
 
-ALTER TABLE top_hospitals ADD COLUMN worse_general_pop_mean DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN worse_general_pop_max DECIMAL;
-ALTER TABLE top_hospitals ADD COLUMN worse_general_pop_min DECIMAL;
+ALTER TABLE top_hospitals ADD COLUMNS (worse_general_pop_mean DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (worse_general_pop_max DECIMAL);
+ALTER TABLE top_hospitals ADD COLUMNS (worse_general_pop_min DECIMAL);
