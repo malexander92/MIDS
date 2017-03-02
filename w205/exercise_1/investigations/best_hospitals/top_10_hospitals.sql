513ede7e3a5adb9526e844751d366d54f4679bb9
Michael Alexander <Malexander@comscore.com>
2017-03-01 22:48:51 -0500

    final top hospitals sql

--- w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
@@ -196,4 +196,6 @@ AND better_hai_measure_count = better_hai_pop_max
 AND better_general_comparison_count = better_general_pop_max
 AND worse_hai_measure_count < worse_hai_pop_mean
 AND worse_general_comparison_count < worse_general_pop_mean
+ORDER BY mortality_agg_score ASC
+LIMIT 10
 ;
