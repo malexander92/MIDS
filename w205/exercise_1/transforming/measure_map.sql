75218953b29158fa6462ef82005f192b0ab5291b
Michael Alexander <Malexander@comscore.com>
2017-03-02 02:46:28 -0500

    map

--- w205/exercise_1/transforming/measure_map.sql
+++ w205/exercise_1/transforming/measure_map.sql
@@ -1,6 +1,6 @@
 -- creating mapping of measure ids to names
-DROP TABLE IF EXISTS measure_map;
-CREATE TABLE measure_map AS
+DROP TABLE IF EXISTS measure_map_pre;
+CREATE TABLE measure_map_pre AS
 SELECT
 	measure_id,
 	measure_name
@@ -8,90 +8,101 @@ FROM measures
 GROUP BY measure_id,measure_name
 ;
 
-INSERT INTO measure_map
+INSERT INTO measure_map_pre
 SELECT
 	a.measure_id,
 	a.measure_name
 FROM effective_care_hospital a
-LEFT OUTER JOIN measure_map b
+LEFT OUTER JOIN measure_map_pre b
 	ON a.measure_id = b.measure_id
 	AND b.measure_id IS NULL
 GROUP BY a.measure_id,a.measure_name
 ;
 
-INSERT INTO measure_map
+INSERT INTO measure_map_pre
 SELECT
 	a.measure_id,
 	a.measure_name
 FROM effective_care_state a
-LEFT OUTER JOIN measure_map b
+LEFT OUTER JOIN measure_map_pre b
 	ON a.measure_id = b.measure_id
 	AND b.measure_id IS NULL
 GROUP BY a.measure_id,a.measure_name
 ;
 
-INSERT INTO measure_map
+INSERT INTO measure_map_pre
 SELECT
 	a.measure_id,
 	a.measure_name
 FROM readmissions_hospital a
-LEFT OUTER JOIN measure_map b
+LEFT OUTER JOIN measure_map_pre b
 	ON a.measure_id = b.measure_id
 	AND b.measure_id IS NULL
 GROUP BY a.measure_id,a.measure_name
 ;
 
-INSERT INTO measure_map
+INSERT INTO measure_map_pre
 SELECT
 	a.measure_id,
 	a.measure_name
 FROM readmissions_state a
-LEFT OUTER JOIN measure_map b
+LEFT OUTER JOIN measure_map_pre b
 	ON a.measure_id = b.measure_id
 	AND b.measure_id IS NULL
 GROUP BY a.measure_id,a.measure_name
 ;
 
-INSERT INTO measure_map
+INSERT INTO measure_map_pre
 SELECT
 	a.measure_id,
 	a.measure_name
 FROM spending_hospital a
-LEFT OUTER JOIN measure_map b
+LEFT OUTER JOIN measure_map_pre b
 	ON a.measure_id = b.measure_id
 	AND b.measure_id IS NULL
 GROUP BY a.measure_id,a.measure_name
 ;
 
-INSERT INTO measure_map
+INSERT INTO measure_map_pre
 SELECT
 	a.measure_id,
 	a.measure_name
 FROM spending_state a
-LEFT OUTER JOIN measure_map b
+LEFT OUTER JOIN measure_map_pre b
 	ON a.measure_id = b.measure_id
 	AND b.measure_id IS NULL
 GROUP BY a.measure_id,a.measure_name
 ;
 
-INSERT INTO measure_map
+INSERT INTO measure_map_pre
 SELECT
 	a.measure_id,
 	a.measure_name
 FROM hai_hospital a
-LEFT OUTER JOIN measure_map b
+LEFT OUTER JOIN measure_map_pre b
 	ON a.measure_id = b.measure_id
 	AND b.measure_id IS NULL
 GROUP BY a.measure_id,a.measure_name
 ;
 
-INSERT INTO measure_map
+INSERT INTO measure_map_pre
 SELECT
 	a.measure_id,
 	a.measure_name
 FROM hai_state a
-LEFT OUTER JOIN measure_map b
+LEFT OUTER JOIN measure_map_pre b
 	ON a.measure_id = b.measure_id
 	AND b.measure_id IS NULL
 GROUP BY a.measure_id,a.measure_name
 ;
+
+DROP TABLE IF EXISTS measure_map;
+CREATE TABLE measure_map AS
+SELECT
+	measure_id,
+	measure_name
+FROM measure_map_pre
+GROUP BY measure_id,measure_name
+;
+
+DROP TABLE IF EXISTS measure_map_pre;
