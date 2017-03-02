8874cdfd4af738a6227ac3af9d7bafd00ac09736
Michael Alexander <Malexander@comscore.com>
2017-03-02 03:27:09 -0500

    sh

--- w205/exercise_1/investigations/run_all_investigation_sql_scripts.sh
+++ w205/exercise_1/investigations/run_all_investigation_sql_scripts.sh
@@ -1,4 +1,4 @@
 # running all investigation sql scripts
-hive -f top_10_hospitals.sql
-hive -f top_10_states.sql
-hive -f top_10_variable_procedures.sql
+hive -f /home/w205/MIDS/w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
+hive -f /home/w205/MIDS/w205/exercise_1/investigations/best_states/top_10_states.sql
+hive -f /home/w205/MIDS/w205/exercise_1/investigations/hospital_variability/top_10_variable_procedures.sql
--- w205/exercise_1/transforming/run_all_transforming_sql_scripts.sh
+++ w205/exercise_1/transforming/run_all_transforming_sql_scripts.sh
@@ -1,12 +1,12 @@
 # running all table creation and transformation sql scripts
-hive -f effective_care_hospitals_scores.sql
-hive -f effective_care_state_scores.sql
-hive -f hai_hospitals_scores.sql
-hive -f hai_state_scores.sql
-hive -f hospital_general_info.sql
-hive -f hospital_general_ratings.sql
-hive -f readmissions_hospitals_scores.sql
-hive -f readmissions_state_scores.sql
-hive -f spending_hospitals_scores.sql
-hive -f spending_state_scores.sql
-hive -f survey_response_data.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/effective_care_hospitals_scores.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/effective_care_state_scores.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/hai_hospitals_scores.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/hai_state_scores.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/hospital_general_info.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/hospital_general_ratings.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/readmissions_hospitals_scores.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/readmissions_state_scores.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/spending_hospitals_scores.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/spending_state_scores.sql
+hive -f /home/w205/MIDS/w205/exercise_1/transforming/survey_response_data.sql
