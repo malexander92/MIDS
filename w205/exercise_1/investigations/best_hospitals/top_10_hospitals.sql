21617b48255f99202a221dc4e326ac3df96654dd
Michael Alexander <Malexander@comscore.com>
2017-03-01 23:00:09 -0500

    sql

--- w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
@@ -192,8 +192,8 @@ SELECT
 FROM top_hospitals
 WHERE readmission_agg_score < readmission_agg_pop_mean
 AND mortality_agg_score < mortality_agg_pop_mean
-AND better_hai_measure_count = better_hai_pop_max
-AND better_general_comparison_count = better_general_pop_max
+AND better_hai_measure_count > better_hai_pop_mean
+AND better_general_comparison_count > better_general_pop_mean
 AND worse_hai_measure_count < worse_hai_pop_mean
 AND worse_general_comparison_count < worse_general_pop_mean
 ORDER BY mortality_agg_score ASC
