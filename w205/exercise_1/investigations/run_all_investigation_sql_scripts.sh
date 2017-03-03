5328b70f533e2deaca366750101e1a519cd821fe
Michael Alexander <Malexander@comscore.com>
2017-03-03 01:44:58 -0500

    sh

--- w205/exercise_1/investigations/run_all_investigation_sql_scripts.sh
+++ w205/exercise_1/investigations/run_all_investigation_sql_scripts.sh
@@ -2,3 +2,4 @@
 hive -f /home/w205/MIDS/w205/exercise_1/investigations/best_hospitals/top_10_hospitals.sql
 hive -f /home/w205/MIDS/w205/exercise_1/investigations/best_states/top_10_states.sql
 hive -f /home/w205/MIDS/w205/exercise_1/investigations/hospital_variability/top_10_variable_procedures.sql
+hive -f /home/w205/MIDS/w205/exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
